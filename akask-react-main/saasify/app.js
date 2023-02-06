5c0e0f89c8b12046fd344e61864437152d039c12
jaganjavid <jagantutor1@gmail.com>
2023-02-06 19:22:56 +0530

    upto sass

--- akask-react-main/saasify/app.js
+++ akask-react-main/saasify/app.js
@@ -2,3 +2,4 @@ const imgHeight = document.querySelector(".dashboard");
 const heightResult = imgHeight.clientHeight / 7;
 document.querySelector("main").style.marginBottom = heightResult + "px";
 
+
