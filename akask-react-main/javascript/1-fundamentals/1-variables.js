8f4fb486bbc496eb4deb62c34509e46e49e6169b
jaganjavid <jagantutor1@gmail.com>
2023-02-10 19:11:21 +0530

    started a var

---
+++ akask-react-main/javascript/1-fundamentals/1-variables.js
@@ -0,0 +1,53 @@
+// Var , Let , Const
+
+// var name = "Javid";
+// name = "Jagan";
+
+console.log(name);
+
+// INIT Variable
+var greeting; // UNdefined
+greeting = "Hello";
+
+console.log(greeting);
+
+
+// Letter, _ , $
+
+// Can not Start with number
+// var 5Star = "Choco";
+// var star5 = "Choco";
+
+// Multi Word Vars
+
+var firstname = "Jagan";
+var firstName = "Jagan"; //camelCase ***
+var first_name = "Jagan"; //snakeCase
+var FirstName = "Jagan Javid"; // Pascal Case
+var $firstName = "Jagan"; // dollar
+
+console.log(firstName);
+
+// Let
+
+let newName;
+newName = "Jack";
+newName = "Jagan Javid";
+
+// Const
+
+// Have to Assign a value
+// const anotherName;
+
+const anotherName = "John";
+
+
+// Have to assign a value
+anotherName = "Jagan";
+
+
+
+console.log(anotherName);
+
+
+
