f003244d1ea4be24b2780120b517dd97aeeb961f
jaganjavid <jagantutor1@gmail.com>
2023-02-21 18:56:20 +0530

    upto select element

---
+++ akask-react-main/javascript/2-dom/2-dom-single-element.js
@@ -0,0 +1,31 @@
+// document.getElementById();
+
+// console.log(document.getElementById("task-title"));
+// console.log(document.getElementById("task-title").id);
+// console.log(document.getElementById("task-title").className);
+
+const taskTitle = document.getElementById("task-title");
+
+// Change Style
+taskTitle.style.background = "blue";
+taskTitle.style.color = "#ffffff";
+taskTitle.style.padding = "15px";
+
+// Change Content
+taskTitle.textContent = "Hello Akash";
+taskTitle.innerText = "Hello Jagan";
+taskTitle.innerHTML = "<p>Hello Jagan Javid</p>";
+
+// document.querySelector() // ***
+
+
+console.log(document.querySelector("#task-title"));
+console.log(document.querySelector(".card-action"));
+console.log(document.querySelector("h5"));
+
+document.querySelector("li").style.color = "blue";
+document.querySelector("ul li").style.color = "brown";
+document.querySelector("li:nth-child(3)").style.color = "orange";
+// document.querySelector("").style.color = "orange";
+// document.querySelector("").style.color = "orange";
+
---
+++ akask-react-main/javascript/2-dom/3-dom-select-multiple-element.js
@@ -0,0 +1,45 @@
+// document.getElementsByClassName();
+
+
+const items = document.getElementsByClassName("collection-item");
+// const tagItems = document.getElementsByTagName("");
+
+// items[0].style.color = "blue";
+// items[0].textContent = "Jagan";
+
+// Convert to array
+
+// let lists = Array.from(items);
+
+
+// lists.forEach(function(element, index, arr){
+//     element.style.color = "blue"
+//     console.log(`${element.textContent} - ${index}`);
+//     console.log(arr);
+// })
+
+// for(let i = 0; i<items.length; i++){
+//    items[i].style.color = "blue";
+// }
+
+
+// const arr = [1,2,3,4,5];
+
+// arr.forEach(function(number){
+//   console.log(number);
+// })
+
+// arr.push(6);
+
+// console.log(arr);
+
+// document.querySelectorAll();
+
+const listItems = document.querySelectorAll("li"); // nodelist
+
+
+listItems.forEach(function(element,index){
+  element.style.color = "blue";
+  console.log(element.textContent);
+  console.log(index);
+})
